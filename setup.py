f4e16b4947afca65f9e01d877a918bc507a1fea0
Sushant-Nemade <65285081+Sushant-Nemade@users.noreply.github.com>
2023-11-08 21:56:14 +0100

    proper structure for pip installation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup (
+    name="math_quiz",
+    version="0.1.0",
+    packages=find_packages(),
+    install_packages=[
+        "matplotlib==3.4.3",
+    ],
+)

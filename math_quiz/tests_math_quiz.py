dc02b91dc4f9294bb80cb84ea60f4b1daa6d5987
Sushant-Nemade <65285081+Sushant-Nemade@users.noreply.github.com>
2023-11-08 21:43:15 +0100

    modified tests_math_quiz.py

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,5 +1,5 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import integerSelector, operatorSelector, calculator
 
 
 class TestMathGame(unittest.TestCase):
@@ -9,22 +9,28 @@ class TestMathGame(unittest.TestCase):
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = integerSelector(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
     def test_function_B(self):
-        # TODO
-        pass
+        operators = ['+', '-', '*']
+
+        for _ in range (1000):
+             rand_operator = operatorSelector()
+             self.assertTrue(rand_operator in operators)
 
     def test_function_C(self):
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (5, 2, '-', '5 - 2', 3),
+                (5, 2, '*', '5 * 2', 10),
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+                problem, answer = calculator(num1, num2, operator)
+                self.assertEqual(problem, expected_problem)
+                self.assertEqual(answer, expected_answer)
+
 
 if __name__ == "__main__":
     unittest.main()
